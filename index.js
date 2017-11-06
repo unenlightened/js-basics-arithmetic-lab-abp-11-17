af658a4f8348da89c16a6790be87e10653e32328
Chely Ho <adept_inept@yahoo.com>
2017-11-06 19:54:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,8 @@ const randomNumber = Math.random() * 20 + 1;
 
 const randomInteger = Math.floor(randomNumber);
 
+<<<<<<< HEAD
 const randomUserID = 1000000000 + randomInteger;
+=======
+const randomUserID = 1000000000 + randomInteger;
+>>>>>>> 9bd4fd9274468ad251438e18fe4222171b3e315f
